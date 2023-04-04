a46e7d6ad5ad564a542878821381f868d60dc435
gavin <1531721786@qq.com>
2023-04-04 15:51:49 +0800

    add train.sh & revise readme

---
+++ train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir -r data/coco 
+mkdir -r data/cocostuffthings
+mkdir  logs #存储运行日志
+
+wget http://images.cocodataset.org/zips/train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget http://calvin.inf.ed.ac.uk/wp-content/uploads/data/cocostuffdataset/stuffthingmaps_trainval2017.zip
+
+unzip -d ./data/coco train2017.zip
+unzip -d ./data/coco val2017.zip
+unzip -d ./data/coco annotations_trainval2017.zip
+unzip -d ./data/cocostuffthings stuffthingmaps_trainval2017.zip
+
+python main.py --base configs/custom_vqgan.yaml -t True --gpus 0,
